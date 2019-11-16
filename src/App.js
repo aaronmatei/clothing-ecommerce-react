537daf43cb0e6dc0a7f9615fa7492c298c22d0b0
aaronmate <aronique@gmail.com>
2019-11-17 00:31:07 +0300

    sign in

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/Homepage.component';
-
-const HatsPage = () => (
-	<div>
-		<h1>HATS PAGE</h1>
-	</div>
-);
+import ShopPage from './pages/shop/ShopPage.component';
+import Header from './components/header/Header.component';
+import Signinsignup from './pages/authentication/Signinsignup.component';
 
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path='/' component={Homepage} />
-				<Route exact path='/hats' component={HatsPage} />
+				<Route exact path='/shop' component={ShopPage} />
+				<Route exact path='/signin' component={Signinsignup} />
 			</Switch>
 		</div>
 	);
---
+++ src/pages/authentication/Signinsignup.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './signinsignup.scss';
+import SignIn from '../../components/sign-in/Sign-in.component';
+
+const Signinsignup = () => {
+	return (
+		<div className='sign-in-and-sign-up'>
+			<SignIn />
+		</div>
+	);
+};
+export default Signinsignup;
